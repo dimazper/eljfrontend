66522dbc785cd238672679802d61d388e11ffc0a
blairdimaz <blair.robson@dia.govt.nz>
2021-03-31 11:51:26 +1300

    fixed css

--- userform/src/components/FormUserDetails.js
+++ userform/src/components/FormUserDetails.js
@@ -3,6 +3,14 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+// import { Checkbox } from 'material-ui';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import './Userform.css';
+
 
 export class FormUserDetails extends Component {
     continue = e => {
@@ -16,6 +24,39 @@ export class FormUserDetails extends Component {
             <MuiThemeProvider>
                 <React.Fragment>
                     <AppBar title="Early Learning Center Form" />
+                   
+    <FormControl component="fieldset">
+      <FormLabel component="legend">What is your full name?</FormLabel>
+      <FormGroup aria-label="position" row>
+        
+        <FormControlLabel
+          value="mr"
+          control={<Checkbox color="primary" />}
+          label="Mr"
+          labelPlacement="end"
+        />
+        <FormControlLabel
+          value="mrs"
+          control={<Checkbox color="primary" />}
+          label="Mrs"
+          labelPlacement="end"
+        />
+        <FormControlLabel
+          value="ms"
+          control={<Checkbox color="primary" />}
+          label="Ms"
+          labelPlacement="end"
+        />
+        <FormControlLabel
+          value="miss"
+          control={<Checkbox color="primary" />}
+          label="Miss"
+          labelPlacement="end"
+        />
+      </FormGroup>
+    </FormControl><br></br>
+                    
+
                     <TextField 
                        hintText="Enter Your First Name" //hfgfg
                        floatingLabelText="First Name"
