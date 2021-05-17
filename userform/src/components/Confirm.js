543eef19b0f28bb86c2b1067b9c87f3a0e3c90bd
blairdimaz <blair.robson@dia.govt.nz>
2021-05-17 14:18:41 +1200

    axios URL changed to catalyst tenancy

--- userform/src/components/Confirm.js
+++ userform/src/components/Confirm.js
@@ -419,11 +419,16 @@ export class Confirm extends Component {
     Child_ece_hourly_fee_2: this.props.values.Child_ece_hourly_fee_2,
     Child_ece_total_weekly_fee_2: this.props.values.Child_ece_total_weekly_fee_2,
     
+
+    // axios.get('http://54.153.158.95:8000/ccs/',
+    // {headers: {'authorization': 'Token 4a5f71da0b2e237c6765c3363e4f18634769ad41'}})
         }
         axios({
             method: 'post',
-            url: 'http://34.230.74.44:8000/ccs/',
-            headers: {'authorization': 'Token 5f1c57dbbe2dbaabe6f8ada1c7f3c0e6dd2e2a35'},
+            url: 'http://54.153.158.95:8000/ccs/',
+            headers: {'authorization': 'Token 4a5f71da0b2e237c6765c3363e4f18634769ad41'},
+            // url: 'http://34.230.74.44:8000/ccs/',
+            // headers: {'authorization': 'Token 5f1c57dbbe2dbaabe6f8ada1c7f3c0e6dd2e2a35'},
             data: body
         })
           .then(function (response) {
